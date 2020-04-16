3b3c5d6100229c7f424dd7193704e4cd03d6bc12
CandyTt20 <458566293@qq.com>
2020-04-16 14:52:42 +0800

    替换nan为平均值

---
+++ mean_replace2.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.arange(12).reshape(3, 4).astype(float)
+a[[1, 2], [1, 2]] = np.nan
+
+def replace(x):
+    for i in range(x.shape[1]): #! 取出一列
+        colum = x[:, i]   #! 设此列为column
+        if np.count_nonzero(colum != colum) != 0: #! 如果这列有nan
+            colum[colum != colum] = colum[colum == colum].mean()
+            #? 将此列平均值赋给nan
+            x[:, i] = colum #? 将替换后的列作为新列
+    return x #? 返回操作后的array
+
+if __name__ == "__main__":
+    print(a)
+    print(replace(a))
