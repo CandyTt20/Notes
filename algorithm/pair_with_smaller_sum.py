30fbe46c4bd469f988c51547cd296bcfe2c54909
CandyTt20 <458566293@qq.com>
2020-05-04 17:27:57 +0800

    pair_with_smaller_sum

---
+++ algorithm/pair_with_smaller_sum.py
@@ -0,0 +1,15 @@
+def pair_with_smaller_sum(arr, target):
+    count = 0
+    arr.sort()
+
+    for left in range(len(arr)-1):
+        right = len(arr)-1
+        while arr[left] + arr[right] >= target and right > left:
+            right -= 1
+        count += right - left
+        
+    return count
+
+arr = [-1, 4, 2, 1, 3]
+target = 5
+print(pair_with_smaller_sum(arr, target))
